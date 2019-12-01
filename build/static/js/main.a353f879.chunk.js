0463e87781fb0d2fe8cd8f2d1bc61eeda76c2c35
Liao Ji Wei <liaojiwei@Liaode-MacBook-Pro.local>
2019-12-01 17:15:19 +0800

    kick off

---
+++ build/static/js/main.a353f879.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpjsx=this.webpackJsonpjsx||[]).push([[0],[,,,function(e,t,n){e.exports=n(4)},function(e,t,n){"use strict";n.r(t);var a=n(0),l=n.n(a),r=n(2);n.n(r).a.render(l.a.createElement((function(){return l.a.createElement("div",null,l.a.createElement("label",{className:"label",htmlFor:"name"},"Enter name:"),l.a.createElement("input",{id:"name",type:"text"}),l.a.createElement("button",{style:{backgroundColor:"blue",color:"white"}},"Click on Me!"))}),null),document.querySelector("#root"))}],[[3,1,2]]]);
+//# sourceMappingURL=main.a353f879.chunk.js.map
